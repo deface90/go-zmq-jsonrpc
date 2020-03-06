259595d4a6cf69499fbba23075c1cfc179bdbf7b
deface90 <deface06@yandex.ru>
2020-03-06 21:38:42 +0500

    added more client tests as usage examples

--- client_test.go
+++ client_test.go
@@ -17,6 +17,22 @@ func (o *ClientObj) Test(args *Args, reply *float64) error {
     return nil
 }
 
+type StringObj string
+func (s *StringObj) Test(str string, reply *string) error {
+    *reply = "Hello, " + str
+    return nil
+}
+
+type MultiObj int64
+func (m *MultiObj) Test(numbers []int64, reply *int64) error {
+    *reply = 1
+    for _, v := range numbers {
+        *reply *= v
+    }
+
+    return nil
+}
+
 func TestClient_Call(t *testing.T) {
     obj := new(ClientObj)
     err := rpc.Register(obj)
@@ -48,6 +64,77 @@ func TestClient_Call(t *testing.T) {
     assert.NoError(t, err)
     assert.Equal(t, nil, resp.Error)
     assert.Equal(t, 30.0, resp.Result)
+
+    err = client.Close()
+    assert.NoError(t, err)
+}
+
+func TestClient_String(t *testing.T) {
+    obj := new(StringObj)
+    err := rpc.Register(obj)
+    assert.NoError(t, err)
+
+    server := &Server{
+        Port: "1234",
+    }
+    err = server.Create()
+    require.NoError(t, err)
+    defer server.Close()
+
+    go server.Serve()
+
+    client := &Client{
+        Port:"1234",
+    }
+    err = client.Create()
+    require.NoError(t, err)
+
+    req := Request{
+        ID:     "10",
+        Method: "StringObj.Test",
+        Params: []interface{}{"World"},
+    }
+
+    resp, err := client.Call(req)
+    assert.NoError(t, err)
+    assert.Equal(t, nil, resp.Error)
+    assert.Equal(t, "Hello, World", resp.Result)
+
+    err = client.Close()
+    assert.NoError(t, err)
+}
+
+func TestClient_Multi(t *testing.T) {
+    obj := new(MultiObj)
+    err := rpc.Register(obj)
+    assert.NoError(t, err)
+
+    server := &Server{
+        Port: "1234",
+    }
+    err = server.Create()
+    require.NoError(t, err)
+    defer server.Close()
+
+    go server.Serve()
+
+    client := &Client{
+        Port:"1234",
+    }
+    err = client.Create()
+    require.NoError(t, err)
+
+    params := []int64{1, 2, 3}
+    req := Request{
+        ID:     "10",
+        Method: "MultiObj.Test",
+        Params: []interface{}{params},
+    }
+
+    resp, err := client.Call(req)
+    assert.NoError(t, err)
+    assert.Equal(t, nil, resp.Error)
+    assert.Equal(t, 6, resp.Result)
 
     err = client.Close()
     assert.NoError(t, err)
